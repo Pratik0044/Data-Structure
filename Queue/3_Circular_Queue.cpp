004462d8ac3e16ebf1a443f78679450344e849be
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-03-02 18:08:04 +0530

    Circular queue in DSA

--- Queue/3_Circular_Queue.cpp
+++ Queue/3_Circular_Queue.cpp
@@ -1,18 +1,101 @@
 // This is a types of queue which is known as CIRCULAR QUEUE
 
-class circularQueue{
+#include<iostream>
+using namespace std;
+
+class CQueue{
     public:
         int rear;
         int front;
         int size;
         int *arr;
-
+    CQueue(int size){
+        rear=-1;
+        front=-1;
+        this->size=size;
+        arr=new int[size];
+        
+    }
     void push(int data){
-        if(){
 
+        // Queue full to nahi hai
+        if((front==0 && rear==size-1) || (rear==front-1)){
+            cout<<"Queue is Full:"<<endl ;
         }
+
+        //Single element ke liye -------> first Element 
+        else if(front == -1){
+            front = rear = 0;
+            arr[rear]=data;
+            cout<<"successfully Pushed element : "<<data<<endl;
+        }
+
+        //Circular nature
+        else if(rear==size-1 && front != 0){
+            rear=0;
+            arr[rear]= data;
+             cout<<"successfully Pushed element : "<<data<<endl;
+        }
+       
+
+        // Normal Flow
         else{
-            
+            rear++;
+            arr[rear]=data;
+             cout<<"successfully Pushed element : "<<data<<endl;
+        }
+
+        
+
+    }
+
+    void pop(){
+        int data=arr[front];
+        // Khali to nahi hai n 
+        if(front==-1){
+            cout<<"Queue is Empty can't POP: "<<endl;
+        }
+
+        // Single Element
+        
+        else if(front== rear){
+            arr[front]=-1;
+            front=-1;
+            rear=-1;
+            cout<<"successfully POP element : "<<data<<endl;
+        }
+
+        //Circular nature
+        
+        else if(front==size-1){
+            arr[front]=-1;
+            front = 0;
+            cout<<"successfully POP element : "<<data<<endl;
+        }
+        
+        // Simple case
+        else{
+            arr[front]=-1;
+            front++;
+            cout<<"successfully POP element : "<<data<<endl; 
         }
     }
+
+    void Getfront(){
+        if(front==-1){
+            cout<<"Queue is Empty : "<<endl;
+        }
+        else{
+        cout<<"Element at front : "<<arr[front]<<endl;
+        }
+    }
+
 };
+
+int main(){
+    CQueue q(10);
+    q.push(1000);
+    q.pop();
+    q.Getfront();
+    
+}
---
+++ Queue/4_DEQueue.cpp
@@ -0,0 +1,4 @@
+// Double Ended Queue
+
+/* ek aisa queue jisame push aur pop dono front and rear dono side se hota hai usse DEQueue kahate hai
+*/
